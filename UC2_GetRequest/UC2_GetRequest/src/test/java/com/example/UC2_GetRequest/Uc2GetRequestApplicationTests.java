1e1166444cd2ab59da51e460eab0bc50b7f6d2f2
Prachiti <prachitiagarkar1@gmail.com>
2025-02-22 11:48:06 +0530

    [Prachiti Agarkar] added: Get request call and add name as query parameter

---
+++ UC2_GetRequest/UC2_GetRequest/src/test/java/com/example/UC2_GetRequest/Uc2GetRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC2_GetRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc2GetRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
